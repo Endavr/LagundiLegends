80796bf3c458360a0fadf2dc6ea787f055f69bf2
jamzer6 <main.jamvincent.guevarra@cvsu.edu.ph>
2025-01-16 05:28:00 +0800

    Merge landing

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Footer from "../components/footer";
 
 const Landing: React.FC = () => {
   return (
     <div>
+      <Navbar />
+      <Hero />
+      <Footer />
     </div>
   );
 };
